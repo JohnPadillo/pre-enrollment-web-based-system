d19733fe93ef592f9e06d5b2c3839fbaad03eac0
John Rafael Padillo <johnrafaelpadillo@gmail.com>
2020-01-20 14:20:34 +0800

    Add Student Page

--- client/src/views/admin/students.vue
+++ client/src/views/admin/students.vue
@@ -1,9 +1,95 @@
 <template>
-  <h1>Students</h1>
+  <v-container fluid>
+    <v-layout>
+      <v-flex>
+        <toolbarNav :title="title">
+          <addButton :title="title" @add="add" />
+        </toolbarNav>
+
+        <addDialog :dialog="openAddDialog" :title="title" ref="addDialog" @close="closeAddDialog">
+          <v-text-field label="Stundent Name" v-model="student_name" outlined></v-text-field>
+          <v-select
+            v-model="student_course"
+            :items="courses"
+            label="Course"
+            item-text="code"
+            item-value="code"
+            outlined
+          ></v-select>
+        </addDialog>
+
+        <v-data-table class="mt-5" :headers="headers" :items="students">
+          <template v-slot:item="props">
+            <tr>
+              <td>{{ props.item.name }}</td>
+              <td>{{ props.item.course }}</td>
+              <td>
+                <editButton />
+                <deleteButton />
+              </td>
+            </tr>
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "Students",
+      openAddDialog: false,
+      student_name: "",
+      student_course: "",
+      students: [
+        {
+          name: "Katrina Castillo",
+          course: "BSIT"
+        },
+        {
+          name: "Vice Ganda",
+          course: "POLSCI"
+        },
+        {
+          name: "Marian Rivera",
+          course: "BSc Psychology"
+        }
+      ],
+      headers: [
+        { text: "Student Name", value: "name" },
+        { text: "Student Course", value: "course" },
+        { text: "Actions" }
+      ],
+      courses: [
+        {
+          code: "BSIT"
+        },
+        {
+          code: "POLSCI"
+        },
+        {
+          code: "BSc Psychology"
+        },
+        {
+          code: "BSED"
+        },
+        {
+          code: "BSCS"
+        }
+      ]
+    };
+  },
+  methods: {
+    add() {
+      this.openAddDialog = true;
+    },
+    closeAddDialog() {
+      this.openAddDialog = false;
+    }
+  }
+};
 </script>
 
 <style></style>
