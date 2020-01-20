6a71c526abb87cca39f9059e8baca1beaeb41da5
John Rafael Padillo <johnrafaelpadillo@gmail.com>
2020-01-20 13:39:56 +0800

    Add Function to Edit and Delete Subject

--- client/src/views/admin/subjects.vue
+++ client/src/views/admin/subjects.vue
@@ -10,29 +10,28 @@
             :title="title"
             ref="addDialog"
             @close="closeAddDialog"
-            @save="addSubject"
+            @save="action === 'edit' ? saveEditSubject() : addSubject()"
           >
             <v-text-field label="Subject Code" v-model="subject_code" outlined></v-text-field>
-            <v-text-field label="Subject Name" v-model="subject_name" outlined></v-text-field>
-            <!-- <v-select
-              v-model="course_department"
-              :items="departments"
-              label="Department"
-              item-text="name"
-              item-value="name"
+            <v-text-field
+              :readonly="action === 'edit'"
+              label="Subject Name"
+              v-model="subject_name"
               outlined
-            ></v-select>-->
+            ></v-text-field>
           </addDialog>
         </toolbarNav>
 
         <v-data-table class="mt-5" :headers="headers" :items="subjects">
           <template v-slot:item="props">
-            <td>{{ props.item.code }}</td>
-            <td>{{ props.item.name }}</td>
-            <td>
-              <editButton />
-              <deleteButton />
-            </td>
+            <tr>
+              <td>{{ props.item.code }}</td>
+              <td>{{ props.item.name }}</td>
+              <td>
+                <editButton @edit="editSubject(props.item)" />
+                <deleteButton @delete="deleteSubject(props.item)" />
+              </td>
+            </tr>
           </template>
         </v-data-table>
       </v-flex>
@@ -45,6 +44,7 @@ export default {
   data() {
     return {
       title: "Subjects",
+      action: "",
       openAddDialog: false,
       subject_name: "",
       subject_code: "",
@@ -52,6 +52,10 @@ export default {
         {
           name: "General Science 1",
           code: "GenSci1"
+        },
+        {
+          name: "Math 1",
+          code: "Math1"
         }
       ],
       headers: [
@@ -71,19 +75,53 @@ export default {
   },
   methods: {
     add() {
+      this.action = "add";
       this.openAddDialog = true;
     },
     closeAddDialog() {
       this.openAddDialog = false;
+      this.reset();
+    },
+    reset() {
+      this.subject_code = "";
+      this.subject_name = "";
     },
     addSubject() {
       let data = {
         name: this.subject_name,
         code: this.subject_code
       };
-
       this.subjects.push(data);
       this.openAddDialog = false;
+      this.reset();
+    },
+    editSubject(data) {
+      this.action = "edit";
+      let subject = this.subjects.find(subject => {
+        return subject.name === data.name;
+      });
+
+      this.subject_code = subject.code;
+      this.subject_name = subject.name;
+      this.openAddDialog = true;
+      console.log(this.action);
+    },
+    saveEditSubject() {
+      let subject = this.subjects.find(subject => {
+        return subject.name === this.subject_name;
+      });
+
+      subject.code = this.subject_code;
+      this.openAddDialog = false;
+      this.reset();
+    },
+
+    deleteSubject(data) {
+      let index = this.subjects.findIndex(subject => {
+        return subject.name === data.name;
+      });
+
+      this.subjects.splice(index, 1);
     }
   }
 };
